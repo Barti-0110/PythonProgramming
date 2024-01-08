46d51215a164278a877e6a7e92a546692bd314b8
Barti-0110 <111067556+Barti-0110@users.noreply.github.com>
2024-01-08 17:15:21 +0100

    Update guessing numbers.py
    Fixed an typo in line 9

--- guessing numbers.py
+++ guessing numbers.py
@@ -6,7 +6,7 @@ random_number = random.randint(number_min, number_max)
 guess_number = None
 
 while guess_number != random_number:
-    gues_number = int(input("Enter number:" ))
+    guess_number = int(input("Enter number:" ))
     if random_number < guess_number:
         print("Too much")
     elif random_number > guess_number:
