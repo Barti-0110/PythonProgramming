8d0981bb1e5c69a5872d826bb1668d37c6f536d7
Barti-0110 <111067556+Barti-0110@users.noreply.github.com>
2024-01-20 17:58:28 +0100

    Update guessing numbers.py
    Added counting attempts to guess

--- guessing numbers.py
+++ guessing numbers.py
@@ -4,13 +4,15 @@ number_max = float(input("Choose Max Number: "))
 random_number = random.randint(number_min, number_max)
 
 guess_number = None
-
+attempts = 0
 while guess_number != random_number:
     guess_number = int(input("Enter number:" ))
+    attempts += 1
     if random_number < guess_number:
         print("Too much")
     elif random_number > guess_number:
         print("Too low")
     else:
+        print(f"Number of attempts {Attempts}")
         print("You guessed the number")
         break
